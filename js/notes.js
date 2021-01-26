21723ab789685cf92dba2452a7b761b20adce122
Godefrox <godefroy1999@gmail.com>
2021-01-26 09:54:16 +0100

    Avancement de note.JS

--- js/notes.js
+++ js/notes.js
@@ -1,11 +1,44 @@
-var main_note = document.getElementsByClassName('note_list_item')[0];
-var add = document.getElementById('add')[0];
-add.addEventListener('onclick',document.write("TEST !!!!! !!! ! !! !"));
-if(add != null){
-    document.write("TO TO LE Chenapan " ) ;
-    add.onclick = function(){
-        document.write("TEST !!!!! !!! ! !! !");
+const noteFormView = {
+    display() {
+        document.getElementById("noteForm").classList.remove("create_edit_note-hidden");
+        document.getElementById("form_add_note_valid").addEventListener("click",noteFormView.validate);
+    },
+    hide(){
+        document.getElementById("noteForm").classList.add("create_edit_note-hidden");
+    },
+    validate(){
+        let titre = document.getElementById("form_add_note_title").value;
+        let contenu = document.getElementById("form_add_note_text").value;
+
+        let note = new Note(titre,contenu);
+        noteView.display(note);
     }
-}else{
-    document.write(" TEST 2  " ) ;
 }
+
+const noteView = {
+
+    convertiseur(note){
+        let conv = new showdown.Converter();
+        let htmlText = conv.makeHtmk(note);
+        return htmlText;
+    },
+    display(titre,contenu) {
+            this.titre = titre;
+            this.contenu = contenu;
+            let variable = ""  + titre + contenu;
+            variable = this.convertiseur(variable);
+            let s = document.getElementsByClassName("main_note").appendChild(variable);
+    }
+
+}
+
+const mainMenuView = {
+    addHandler(event){
+        noteFormView.display();
+    },
+    init(){
+        document.getElementById("add").addEventListener("click",this.addHandler());
+    },
+}
+
+window.addEventListener("load", mainMenuView.init());
